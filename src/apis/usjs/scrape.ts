fe450a9b709b0570a70018a64ccdf4c49aad666a
jenell <Jenell.E.Rotter@phila.gov>
2025-11-18 12:17:14 -0500

    updated the randomized user agents to choose from an array rather than randomizing the version numbers, some of which may not exist irl

--- src/apis/usjs/scrape.ts
+++ src/apis/usjs/scrape.ts
@@ -33,8 +33,16 @@ const downloadFile = ({ type }: DocumentType) => async (acc: RestAccumulator): P
   page.setDefaultTimeout(60000); // 60 seconds
   page.setDefaultNavigationTimeout(60000); // 60 seconds
 
+  const userAgents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.1 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
+    // Add more user agents as needed
+  ];
+  const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
   await page.setExtraHTTPHeaders({
-    'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.${Math.floor(Math.random()*100)} Safari/537.36`
+    'User-Agent': randomUserAgent
   });
   
   // Anti-detection: Random delay
